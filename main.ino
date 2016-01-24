d51a78696bf019cb529b71d6ecf8f8241947a920
AdamLuptak <adamluptakosice@gmail.com>
2016-01-24 22:17:56 +0100

    Starting point of project

---
+++ main.ino
@@ -0,0 +1,29 @@
+/*
+	Tap tempo project
+
+	Calculate tempo in BPM from avarage button taps with button ( 4 times / 4 ) from millisecond
+
+	The circuit:
+	* Pin 2 button 5V, 10k to ground 
+	* Pin 5V to button
+
+	Created Sunday January 2016
+	By Adam Luptak
+	Modified Sunday January 2016
+	By Adam Luptak
+	Version: 1.0.
+
+	https://github.com/AdamLuptak/TapTempoProject.git
+
+*/
+#define TAP_BUTTON_INPUT = 2 // attached to interupt
+
+void setup()
+{
+	//initialize 
+}
+
+void loop()
+{
+	Serial.println("idem uz");
+}
